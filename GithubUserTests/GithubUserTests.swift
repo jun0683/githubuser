d448217e29ab84e2a34a94a47934c5832dfabd7f
hongjunkim <hongjunkim@smilegate.com>
2022-06-22 00:24:40 +0900

    테스트 코드 추가

--- GithubUserTests/GithubUserTests.swift
+++ GithubUserTests/GithubUserTests.swift
@@ -18,9 +18,109 @@ class GithubUserTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        //given
-        //when
-        //then
+    func test_검색화면에서_응답에러() throws {
+        // given
+        let workerMock = GithubUserSearchWorkerMock(db: GitHubUserDB.shared)
+        let interactor = GitHubUserSearchInteractor(worker: workerMock)
+        let presenter = GitHubUserSearchPresenter()
+        let viewControllerMock = GitHubUserSearchDisplayMock()
+        interactor.presenter = presenter
+        presenter.viewController = viewControllerMock
+
+        // when
+        workerMock.searchResult = .failure(NSError(domain: "", code: -1))
+        interactor.searchUser(request: .init(name: "test"))
+
+        // then
+        XCTAssertEqual((viewControllerMock.displayError as? NSError)?.code, -1)
+    }
+    
+    func test_검색화면에서_응답성공() throws {
+        // given
+        let workerMock = GithubUserSearchWorkerMock(db: GitHubUserDB.shared)
+        let interactor = GitHubUserSearchInteractor(worker: workerMock)
+        let presenter = GitHubUserSearchPresenter()
+        let viewControllerMock = GitHubUserSearchDisplayMock()
+        interactor.presenter = presenter
+        presenter.viewController = viewControllerMock
+
+        // when
+        workerMock.searchResult = .success(.init(totalCount: 2,
+                                                 incompleteResults: true,
+                                                 items: [.init(login: "test1", id: 0, avatarURL: "https://dramancompany.com"),
+                                                         .init(login: "test2", id: 1, avatarURL: "https://dramancompany.com")]))
+        interactor.searchUser(request: .init(name: "test"))
+
+        // then
+        XCTAssertEqual(viewControllerMock.displaySearchUserViewModel?.userList.first?.name, "test1")
+        XCTAssertEqual(viewControllerMock.displaySearchUserViewModel?.userList.last?.name, "test2")
+    }
+    
+    func test_검색화면에서_검색후_즐겨찾기() throws {
+        // given
+        let workerMock = GithubUserSearchWorkerMock(db: GitHubUserDB.shared)
+        let interactor = GitHubUserSearchInteractor(worker: workerMock)
+        let presenter = GitHubUserSearchPresenter()
+        let viewControllerMock = GitHubUserSearchDisplayMock()
+        interactor.presenter = presenter
+        presenter.viewController = viewControllerMock
+
+        // when
+        workerMock.searchResult = .success(.init(totalCount: 2,
+                                                 incompleteResults: true,
+                                                 items: [.init(login: "test1", id: 0, avatarURL: "https://dramancompany.com"),
+                                                         .init(login: "test2", id: 1, avatarURL: "https://dramancompany.com")]))
+        interactor.searchUser(request: .init(name: "test"))
+        let user = viewControllerMock.displaySearchUserViewModel!.userList.first!
+        interactor.favoriteUser(request: .init(user: user))
+
+        // then
+        XCTAssertEqual(viewControllerMock.displaySearchUserViewModel?.userList.first?.favorite, true)
+        XCTAssertEqual(viewControllerMock.displaySearchUserViewModel?.userList.last?.favorite, false)
+    }
+}
+
+class GithubUserSearchWorkerMock: GitHubUserSearchWorkerProtocol {
+    var db: DBProtocol
+    var searchResult: Result<SearchResultModel, Error>?
+    
+    init(db: DBProtocol) {
+        self.db = db
+    }
+    
+    func searchUser(name: String, completion: @escaping (Result<SearchResultModel, Error>) -> Void) {
+        if let searchResult = searchResult {
+            completion(searchResult)
+        }
+    }
+    
+    func setFavoriteID(user: User) {
+        db.setFavoriteID(user: user)
+    }
+    
+    func getFavoriteID(user: User) -> Bool {
+        db.getFavoriteID(user: user)
+    }
+    
+    func getFavoriteUserList() -> [User] {
+        db.getFavoriteUserList()
+    }
+    
+}
+
+class GitHubUserSearchDisplayMock: GitHubUserSearchDisplayLogic {
+    var displaySearchUserViewModel: GitHubUserSearch.SearchUser.ViewModel?
+    var displayError: Error?
+    
+    func displaySearchUser(viewModel: GitHubUserSearch.SearchUser.ViewModel) {
+        displaySearchUserViewModel = viewModel
+    }
+    
+    func displayUpdateFavoriteUser() {
+        
+    }
+    
+    func displayError(error: Error) {
+        displayError = error
     }
 }
